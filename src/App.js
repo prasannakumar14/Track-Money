e7c532809c83923234c0d53f53f29ccf7a504cd3
prasannakumar14 <palevallakumar@gmail.com>
2022-01-31 09:46:52 +0530

    Track Money

--- src/App.js
+++ src/App.js
@@ -6,71 +6,53 @@ import './App.css'
 
 
 class App extends Component{
-  state ={name: "",amount: "", total: 0}
+  state ={name: "",amount: "", total: 0, select:"Credit"}
 
   onChangeInput = event =>{
-    this.setState({amount:event.target.value})
+    this.setState({amount: event.target.value})
   }
 
   onChangeName = event =>{
-    this.setState({name:event.target.value })
+    this.setState({name: event.target.value })
+  }
+
+  onChangeOption = event =>{
+    this.setState({select: event.target.value})
   }
   
 
 //  
-  creditAmount =() =>{
-    const {name, amount, total} = this.state
-    if(name === "" || amount === ""){
-      alert("Enter valid Name and Amount")
-    }else{
+  
+
+  onSubmit = () =>{
+    const {name, amount, total, select} = this.state;
+
     let update = localStorage.getItem("Todo")
     let parssed = JSON.parse(update)
     let date = new Date()
     let fullDate = `${date.getDate()}-${date.getMonth()+1}-${date.getFullYear()}`
-    const updateData = {
-      name: name,
-      amount: parseInt(amount),
-      credit: parseInt(amount),
-      debit : "",
-      date: fullDate,
-      totalAmount: parseInt(amount) + total
-    }
+
     if(parssed == null){
       parssed =[]
     }
-    parssed.push(updateData)
-    localStorage.setItem('Todo', JSON.stringify(parssed))
-    this.setState(prevState =>({total: prevState.total+parseInt(amount),name:"", amount:""}))
-  }
-  }
 
-
-//
-  debitAmount = () =>{
-    const {name, amount, total} = this.state
-    if(name === "" || amount === ""){
-      alert("Enter valid Name and Amount")
-    }else{
-    let update = localStorage.getItem("Todo")
-    let parssed = JSON.parse(update)
-    let date = new Date()
-    let fullDate = `${date.getDate()}-${date.getMonth()+1}-${date.getFullYear()}`
     const updateData = {
+      id:parssed.length+1,
       name: name,
-      amount: parseInt(amount),
-      credit: "",
-      debit : parseInt(amount),
+      amount: `${parseInt(amount)}.00`,
+      credit: (select === "Credit")? `${parseInt(amount)}.00`: "",
+      debit : (select === "Debit")? `${parseInt(amount)}.00`: "",
       date: fullDate,
-      totalAmount: total -parseInt(amount)
+      totalAmount: (select === "Credit") ? `${parseInt(amount) + total}.00`: `${total -parseInt(amount)}.00`
     }
-    if(parssed == null){
-      parssed =[]
-    }
+    
     parssed.push(updateData)
     localStorage.setItem('Todo', JSON.stringify(parssed))
-    this.setState(prevState =>({total: prevState.total-parseInt(amount),name:"", amount:""}))
+    this.setState(prevState =>({total: (select === "Credit") ?prevState.total+parseInt(amount):prevState.total-parseInt(amount),name:"", amount:""}))
+  
   }
-  }
+
+
 
 
   //
@@ -86,6 +68,7 @@ class App extends Component{
     return(
       <table>
        <tr>
+          <th>S.No</th>
           <th>Name</th>
           <th>Amount</th>
           <th>Credit</th>
@@ -96,6 +79,7 @@ class App extends Component{
     {data.map(every =>{
       return(
       <tr>
+        <td>{every.id}</td>
         <td>{every.name}</td>
         <td>{every.amount}</td>
         <td className="credit">{every.credit}</td>
@@ -118,6 +102,7 @@ class App extends Component{
     let change = 0
     let credit =0
     let debit =0
+
     if(parsedAmount === null){
       this.setState({total: change})
     }
@@ -127,35 +112,40 @@ class App extends Component{
        console.log(credit);
        console.log(debit)
        change = credit - debit
-
     }
     
      this.setState({total:change})
   }
 
+
+
  // 
   render(){
-    const {name,amount,total} = this.state
+    const {name,amount,total, select} = this.state
     
     return(
       <div className="main-container">
-        <h1>Track Your Expendeture</h1>
-        <h3>Your Total Amount: {total}</h3>
-        <form className="form-container">
-        <div className="amount-container">
-          <MdOutlineDriveFileRenameOutline/>
-        <input type="text" placeholder="Enter Name" className="input-form" value={name} onChange={this.onChangeName}/>
-        </div>
-        <div className="amount-container">
-          <FaRupeeSign className="rupee-icon"/>
-          <input type="text" placeholder="Enter Amount" className="input-form" value={amount} onChange={this.onChangeInput}/>
-          </div>
+        <h1>Track Your Expenditure</h1>
+        <h3>Your Total Amount: {total}.00</h3>
+        <form className="form-container" onSubmit={this.onSubmit}>
+            <div className="amount-container">
+                <MdOutlineDriveFileRenameOutline />
+                <input type="text" placeholder="Enter Name" className="input-form" value={name} onChange={this.onChangeName} required/>
+            </div>
+            <div className="amount-container">
+                <FaRupeeSign className="rupee-icon"/>
+                <input type="text" placeholder="Enter Amount" className="input-form" value={amount} onChange={this.onChangeInput} required/>
+            </div>
+            <div >
+                <select onChange={this.onChangeOption} className="option-container" value={select}>
+                    <option value="Credit">Credit</option>
+                    <option value="Debit">Debit</option>
+                </select>
+            </div>
+            <button className="button" type="submit">ADD</button>
         </form>
-        <div className="button-container">
-          <button type="button" className="button" onClick={this.creditAmount}>CREDIT</button>
-          <button type="button" className="button" onClick={this.debitAmount}>DEBIT</button>
-          </div>
-         {this.renderData()}
+        
+        {this.renderData()}
               
       </div>
     )
